d547ddb64618a642384ab2fc6d90ed0e9a8b1005
slk <slk.mstu@mail.ru>
2025-11-01 21:33:55 +0300

    Initial commit: setup HabitHero project

---
+++ HabitHero/HabitHero/HabitHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitHeroApp.swift
+//  HabitHero
+//
+//  Created by Сулейман Курбанов on 01.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitHero/HabitHeroTests/HabitHeroTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitHeroTests.swift
+//  HabitHeroTests
+//
+//  Created by Сулейман Курбанов on 01.11.2025.
+//
+
+import Testing
+@testable import HabitHero
+
+struct HabitHeroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
